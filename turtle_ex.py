45a6e6492ec9102e947f6de7f1c3bec3e21d6779
Stefan K <40999112+s-tefan@users.noreply.github.com>
2026-01-22 21:35:52 +0100

    turtle_ex

---
+++ turtle_ex.py
@@ -0,0 +1,29 @@
+import turtle
+
+# Utgångsläge koordinater (0,0), riktning 0 grader = höger/österut
+
+# Vi definierar en funktion som ritar lite
+def rita_lite():
+
+    turtle.pendown() # sätt ner pennan
+    turtle.forward(100) # flytta framåt 100 pixlar
+    turtle.left(120) # vrid vänster 120 grader
+    turtle.forward(100)
+    turtle.left(120)
+    turtle.forward(100)
+    turtle.penup() # lyft pennan
+
+
+    # Vad gör följande kod?
+    turtle.goto(100, -100) # gå till punkt med koordinater (100, -100)
+    turtle.setheading(-45) # sätt riktning till -45 grader (sydost)
+    turtle.pendown() # sätt ner pennan
+    turtle.begin_fill() 
+    # fyll i innanför konturen
+    for k in range(4):
+        turtle.forward(50)
+        turtle.right(90)
+    turtle.end_fill()
+
+rita_lite()
+input("Tryck enter för att avsluta")
