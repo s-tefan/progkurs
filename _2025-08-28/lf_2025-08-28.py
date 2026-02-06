b5caef21bb404670d4279ddc96ee84097c68848c
Stefan K <40999112+s-tefan@users.noreply.github.com>
2026-02-06 16:58:48 +0100

    -

--- _2025-08-28/lf_2025-08-28.py
+++ _2025-08-28/lf_2025-08-28.py
@@ -1,6 +1,8 @@
-import CoreData
+from CoreData import sensor_data # Importerar sensor_data från separat fil CoreData.py
 
 def all_messages_about_product(data, product_id):
+    ''' Plockar ut alla dataposter med givet värde på 'product'.
+    Sorteras i tidsordning. '''
     ret=[] 
     for i in data:
         if i["product"]==product_id: 
@@ -35,7 +37,7 @@ def test_sensors(data, product_id):
     for s in [{1,2,3,4},{1,2,5,6}]:
         if sensor_set == s:
             return -1
-        elif sensor_set < s: # A<B betyder för mängder att A är en äkta delmängd till B, dva att varje element i A också finns i B, men att finns minst ett element i B som inte finns i A.
+        elif sensor_set < s: # A<B betyder för mängder att A är en äkta delmängd till B, dvs att varje element i A också finns i B, men att finns minst ett element i B som inte finns i A.
             return (s - sensor_set).pop() # ger en sensor som felat, även om det var fler
 
 
@@ -45,16 +47,16 @@ def list_error_codes(data):
         print(product, test_sensors(data, product))
 
 
-list_error_codes(CoreData.sensor_data)
+list_error_codes(sensor_data)
 
 def vg(data):
-    candidates = set(mess["sensor"] for mess in CoreData.sensor_data ) 
-    for product in set(m["product"] for m in data):
-        sensor_list = sorted(mess["sensor"] for mess in all_messages_about_product(data, product))
-        # obs: mess_list är i tidsordning NEJNEJNEJ
-        for k in range(1,len(sensor_list)):
-            if sensor_list[k] < sensor_list[k-1]:
-                print("Sensorer", sensor_list[k-1], sensor_list, "rapporterade i fel ordning för produkt", product)
-                candidates &= {sensor_list, sensor_list} 
+    candidates = set(mess["sensor"] for mess in sensor_data ) # Mängd initierad med alla sensor-nr
+    for product in set(m["product"] for m in data): # En produkt i taget
+        messes = all_messages_about_product(data, product) # redan sorterad med avseende på tid
+        for s in range(1,len(messes)):
+            for f in range(s):
+                first, second = messes[f], messes[s] # first kommer före second i tid
+                if first['sensor'] > second['sensor']: # om sensorerna meddelat i fel tidsordning
+                    candidates &= {first['sensor'], second['sensor']} # behåll sensornumren  
     return candidates
 print("Följande sensorer förekommer i alla meddelanden där sensorerna inte rapporterats i rätt ordning:", vg(CoreData.sensor_data))
